75e3c8b573b576f0806267b57fe6210f27867e33
Tyler Bertrand <tbertrand@gradle.com>
2023-07-28 13:23:58 -0500

    publish build scan only if authenticated

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,9 @@
 plugins {
-    id("com.gradle.enterprise") version("3.14.1")
+    id("com.gradle.enterprise") version ("3.14.1")
     id("com.gradle.common-custom-user-data-gradle-plugin") version ("1.11.1")
 }
 
-gradleEnterprise {
-    server = "https://ge.solutions-team.gradle.com"
-    buildScan {
-        publishAlways()
-        isUploadInBackground = System.getenv("CI") == null
-        capture {
-            isTaskInputFiles = true
-        }
-    }
-}
+apply(from = "ge.gradle")
 
 rootProject.name = "spotless-reproducer"
 include(":lib")
